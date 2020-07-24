57f1295283af6fdf6c46737fbf4e6c9b9b2a47fb
Kqliber <Dylansaidler03@gmail.com>
2020-07-24 01:56:27 +0100

    added block break listener

---
+++ src/main/kotlin/me/clip/ezblocks/listeners/BlockBreak.kt
@@ -0,0 +1,30 @@
+package me.clip.ezblocks.listeners
+
+import me.clip.ezblocks.getValue
+
+import org.bukkit.Material
+import org.bukkit.event.Listener
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.block.BlockBreakEvent
+
+/**
+ * Listens to BlockBreakEvent and adds to block counter.
+ *
+ * @author Kaliber
+ */
+
+class BlockBreak : Listener {
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    fun onBlockBreak(event: BlockBreakEvent) {
+        val block = event.block.blockData.material
+        val excludedBlocks = getValue<List<String>>("excluded_blocks").map { Material.getMaterial(it) }
+        if (block in excludedBlocks) {
+            return
+        }
+        val blockCounter = 1
+        // TODO: add blockCounter to the player's blocks mined statistic in database
+    }
+
+}
