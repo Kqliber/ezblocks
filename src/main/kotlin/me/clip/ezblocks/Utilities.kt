4dd270f1dfd99ad79c23b89eb35be136128d9eec
Kqliber <Dylansaidler03@gmail.com>
2020-08-11 01:01:02 +0100

    placeholderapi support

--- src/main/kotlin/me/clip/ezblocks/Utilities.kt
+++ src/main/kotlin/me/clip/ezblocks/Utilities.kt
@@ -1,5 +1,9 @@
 package me.clip.ezblocks
 
 import org.bukkit.ChatColor
+import org.bukkit.OfflinePlayer
+import me.clip.placeholderapi.PlaceholderAPI
 
 fun String.colourise(): String = ChatColor.translateAlternateColorCodes('&', this)
+
+fun setMessage(player: OfflinePlayer, message: String) = PlaceholderAPI.setPlaceholders(player, message.colourise())
