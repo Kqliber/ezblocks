0a0a09f928b24b343516c35038fe8348310d8af2
Kqliber <Dylansaidler03@gmail.com>
2020-09-13 23:32:01 +0100

    /blocks command with player arg support (hopefully)

---
+++ src/main/kotlin/me/clip/ezblocks/commands/BlockCounterCommand.kt
@@ -0,0 +1,35 @@
+package me.clip.ezblocks.commands
+
+import me.clip.ezblocks.getValue
+import me.clip.ezblocks.sendTranslatedMessage
+import me.clip.ezblocks.handlers.PlayerDataHandler
+
+import me.mattstudios.mf.base.CommandBase
+import me.mattstudios.mf.annotations.Command
+import me.mattstudios.mf.annotations.Default
+import me.mattstudios.mf.annotations.Optional
+import org.bukkit.Bukkit
+
+import org.bukkit.entity.Player
+import org.bukkit.command.CommandSender
+
+@Command("blocks")
+class BlockCounterCommand : CommandBase() {
+
+    @Default
+    fun blocksCommand(sender: CommandSender, @Optional playerArg: String?) {
+        val player = sender as Player
+        val data = PlayerDataHandler()
+
+        if (playerArg == null) {
+            val message = getValue<String>("messages.blocks_broken").replace("%blocks%", "${data.getBlocks(player)}")
+
+            return player.sendTranslatedMessage(player, message)
+        }
+
+        val player2 = Bukkit.getOfflinePlayer(playerArg) as Player
+        val message = getValue<String>("messages.blocks_broken").replace("%blocks%", "${data.getBlocks(player2)}")
+
+        player.sendTranslatedMessage(player2, message)
+    }
+}
